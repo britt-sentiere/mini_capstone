d37583588e830f0a7bf0b1a8b703ba9f20a1c39d
brittani <britt.sentiere@gmail.com>
2019-08-29 12:06:12 -0500

    final edits for day

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,12 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products_view.json.jb'
   end 
+
+  def first_product
+  @product = Product.first
+  render 'first_product_view.json.jb'
+  end
+  
 end
+
+
