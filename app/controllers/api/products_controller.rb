e63da1b7bcfbf3d37b121729b04d44a159445ddb
brittani <britt.sentiere@gmail.com>
2019-09-03 10:34:45 -0500

    restful routing

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,14 +5,40 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jb'
   end 
 
-  # def create
-  # end
+   def create
+    @product = Product.new(
+                          id: params[:id],
+                          name: params[:name],
+                          price: params[:price],
+                          image_url: params[:image_url],
+                          description: params[:description]
+                          )
+    @product.save
+    render 'show.json.jb'
+  end 
 
   def show 
     @product = Product.find(params[:id])
     render 'show.json.jb'
   end 
-  
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render 'show.json.jb'
+  end 
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "Successfully Destroyed Product"}
+  end
 end
 
 
