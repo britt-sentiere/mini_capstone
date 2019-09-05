9e7d61a81b2ee8d3b76ad3005336427cf5a5d8f2
brittani <britt.sentiere@gmail.com>
2019-09-05 12:31:20 -0500

    thursday

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { maximum: 500, minimum: 10  } # { in: 10..500 } could also work
+  validates :description, presence: true
+  validates :image_url, presence: true
+
   def is_discounted?
     price < 3
   end 
